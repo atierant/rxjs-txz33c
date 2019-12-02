63767ce87bfb5d86147d4ba63bdd1268f9619df1
atierant <arnaud@tierant.fr>
2019-12-02 17:44:52 +0100

    RxJS PlayGround

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
-import { of } from 'rxjs'; 
-import { map } from 'rxjs/operators';
+// Import stylesheets
+import './style.css';
+import { Observable, range, of, interval, fromEvent, from, throwError, empty } from 'rxjs';
+import { filter, map, flatMap, scan, take, tap, buffer, debounceTime, catchError } from 'rxjs/operators';
 
+var input = document.getElementById('textInput');
 
-const source = of('World').pipe(
-  map(x => `Hello ${x}!`)
-);
+var inputObservable = fromEvent<KeyboardEvent>(input, 'keypress')
+  .pipe(
+   map(event => event.key),
+   filter(x => x.match(/^[a-zA-Z]+$/)),
+   map(c => c.toUpperCase)
+  )
 
-source.subscribe(x => console.log(x));
+inputObservable.subscribe(x => sendValues(x));
+
+function sendValues(x){
+  var pre = document.createElement('pre');
+  pre.innerHTML = JSON.stringify(x);
+  document.getElementById('results').appendChild(pre);
+}
